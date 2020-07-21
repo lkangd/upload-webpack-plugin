b2ce0f40e2b46eb9945d2e4bee03ce18b04f6e9e
CurtisLiong <lkangd@gmail.com>
2020-07-22 04:17:08 +0800

    feat(sort-upload-order.js): add sortUploadOrder method
    添加上传资源列表的排序函数,被引用的资源会优先上传,根据需要替换已上传资源在别的文件的引用.

---
+++ src/helpers/sortUploadOrder.js
@@ -0,0 +1,24 @@
+export default function sortUploadOrder(filenames = [], extOrderTemplate = ['.css', '.js', '.html']) {
+  const levelMap = new Map();
+  const extOrder = extOrderTemplate.map((ext, i) => {
+    const extRegExp = new RegExp(`\\${ext}$`);
+    levelMap.set(extRegExp, i);
+    return extRegExp;
+  });
+  const all = filenames.sort((a, b) => {
+    const matchExtB = extOrder.find(ext => ext.test(b));
+    if (matchExtB) {
+      const extLevelB = levelMap.get(matchExtB);
+      const matchExtA = extOrder.find(ext => ext.test(a));
+      if (matchExtA) {
+        const extLevelA = levelMap.get(matchExtA);
+        if (extLevelB < extLevelA) return 1;
+      }
+      return -1;
+    }
+    return 1;
+  });
+  let extEdge = all.findIndex(filename => extOrder.find(ext => ext.test(filename)));
+  extEdge = extEdge === -1 ? all.length : extEdge;
+  return { all, priority: all.slice(0, extEdge), nonPriority: all.slice(extEdge) };
+}
