2dce6af2a7956be7d26cadea52b04869ae885dc4
CurtisLiong <lkangd@gmail.com>
2020-08-04 17:44:33 +0800

    test: optimize option enable test

--- test/UploadPlugin/option-enable.spec.js
+++ test/UploadPlugin/option-enable.spec.js
@@ -17,15 +17,17 @@ describe('UploadPlugin:option-enable', () => {
   });
   test('should do noting when option "enable" equal "false"', done => {
     const uploader = uploaders.sync();
-    const webpackConfig = getWebpackConfig(new UploadPlugin({ uploader, options: { muteLog: false, enable: false } }));
+    const mockFn = jest.fn();
+    // eslint-disable-next-line func-names
+    const uploaderMockFn = function (...args) {
+      mockFn(...args);
+      return uploader(...args);
+    };
+    const webpackConfig = getWebpackConfig(new UploadPlugin({ uploader: uploaderMockFn, options: { muteLog: false, enable: false } }));
     const compiler = webpack(webpackConfig, function callback(error, result) {
       expect(error).toBeFalsy();
       expect(result.compilation.errors.length).toBe(0);
-      // const js = result.compilation.assets['file.js'].source();
-      // eval(js);
-      // const scripts = document.head.getElementsByTagName('script');
-      // expect(scripts.length).toBe(1);
-      // expect(scripts[0].src).toBe('http://localhost/0.js');
+      expect(mockFn).not.toHaveBeenCalled();
       done();
     });
     compiler.outputFileSystem = new MemoryFs();
