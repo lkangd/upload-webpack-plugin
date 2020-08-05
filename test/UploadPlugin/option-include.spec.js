bdebb1237ac67e9568d5d763e2fe1f2277edfaed
CurtisLiong <lkangd@gmail.com>
2020-08-05 16:14:56 +0800

    test: optimize option include test

--- test/UploadPlugin/option-include.spec.js
+++ test/UploadPlugin/option-include.spec.js
@@ -17,23 +17,30 @@ describe('UploadPlugin:option-include', () => {
   });
   test('should replace all returned url', done => {
     const uploader = uploaders.sync();
+    const mockFn = jest.fn(uploader);
+    // eslint-disable-next-line func-names
+    const uploaderMockFn = function (...args) {
+      return mockFn(...args);
+    };
+    const include = ['index.html', /\.ttf$/, /\.js$/, /^dynamicSub\..*\.bundle\.js$/, /^dynamicSub\..*bundle\.css$/];
     const webpackConfig = getWebpackConfig(
       new UploadPlugin({
-        uploader,
+        uploader: uploaderMockFn,
         options: {
           muteLog: true,
-          include: ['index.html', /\.ttf$/, /\.js$/, /^dynamicSub\..*\.bundle\.js$/, /^dynamicSub\..*bundle\.css$/],
+          include,
         },
       }),
     );
     const compiler = webpack(webpackConfig, function callback(error, result) {
       expect(error).toBeFalsy();
       expect(result.compilation.errors.length).toBe(0);
-      // const js = result.compilation.assets['file.js'].source();
-      // eval(js);
-      // const scripts = document.head.getElementsByTagName('script');
-      // expect(scripts.length).toBe(1);
-      // expect(scripts[0].src).toBe('http://localhost/0.js');
+      expect(mockFn).toHaveBeenCalledTimes(11);
+      Object.keys(result.compilation.assets).forEach(filename => {
+        if (include.some(flag => flag === filename || (flag.test && flag.test(filename)))) {
+          expect(mockFn).toHaveBeenCalledWith(result.compilation.assets[filename].source(), filename);
+        }
+      });
       done();
     });
     compiler.outputFileSystem = new MemoryFs();
